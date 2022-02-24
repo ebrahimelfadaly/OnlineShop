dd542ab4480f2cbcbf281e9a493361c55f4437ab
rania-265 <79195419+rania-265@users.noreply.github.com>
2022-02-24 10:21:53 -0800

    category
    category

---
+++ app/src/main/java/com/example/onlineshop/ui/entry/WeclomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.onlineshop.ui.entry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.onlineshop.R
+
+class WeclomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weclome)
+    }
+}
