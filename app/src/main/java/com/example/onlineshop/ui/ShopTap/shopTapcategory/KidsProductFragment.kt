33361e479d903c354bd765b20b93641a55a1bbbc
ebrahimelfadaly <elfadalyebrahim@gmail.com>
2022-02-24 17:22:29 +0200

    ebrahim

---
+++ app/src/main/java/com/example/onlineshop/ui/ShopTap/shopTapcategory/KidsProductFragment.kt
@@ -0,0 +1,29 @@
+package com.example.onlineshop.ui.ShopTap.shopTapcategory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.onlineshop.R
+
+
+class KidsProductFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_kids_product, container, false)
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/onlineshop/ui/ShopTap/shopTapcategory/WomanProductFragment.kt
@@ -0,0 +1,27 @@
+package com.example.onlineshop.ui.ShopTap.shopTapcategory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.onlineshop.R
+
+
+class WomanProductFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_woman_product, container, false)
+    }
+
+
+}
