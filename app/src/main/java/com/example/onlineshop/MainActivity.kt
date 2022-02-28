d39818cead1535618fbd3f2cd867ab10f6d2dd39
enasalosta <96724284+enasalosta@users.noreply.github.com>
2022-02-28 18:20:24 +0200

    commit2

--- app/src/main/java/com/example/onlineshop/MainActivity.kt
+++ app/src/main/java/com/example/onlineshop/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-
+        var x=5
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         navView.setupWithNavController(navController)
