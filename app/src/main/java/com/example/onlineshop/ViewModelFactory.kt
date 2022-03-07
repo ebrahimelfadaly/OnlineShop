4e59a2b9df36aa1719ab51b568f5c681d53e7cb1
enasalosta <96724284+enasalosta@users.noreply.github.com>
2022-03-07 15:20:40 +0200

    login/register

--- app/src/main/java/com/example/onlineshop/ViewModelFactory.kt
+++ app/src/main/java/com/example/onlineshop/ViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.onlineshop.repository.IRepository
 import com.example.onlineshop.ui.ShopTap.ShopViewModel
+import com.example.onlineshop.ui.login_register.ui.login.LoginViewModel
 
 
 class ViewModelFactory(private val repositoryImpl: IRepository,private val application: Application):ViewModelProvider.Factory {
@@ -12,6 +13,9 @@ class ViewModelFactory(private val repositoryImpl: IRepository,private val appli
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(ShopViewModel::class.java)) {
             return ShopViewModel(repositoryImpl, application) as T
+        }
+        else if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(repositoryImpl,application) as T
         }
         else {
             throw IllegalArgumentException("ViewModel Not Found")
--- app/src/main/java/com/example/onlineshop/ui/login_register/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/onlineshop/ui/login_register/ui/login/LoginFragment.kt
@@ -1,5 +1,7 @@
 package com.example.onlineshop.ui.login_register.ui.login
 
+import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.annotation.StringRes
@@ -15,11 +17,181 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
-//import com.example.onlineshop.ui.login_register.databinding.FragmentLoginBinding
-
-//import com.example.onlineshop.ui.login_register.R
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.onlineshop.R
+import com.example.onlineshop.ViewModelFactory
+import com.example.onlineshop.data.entity.customer.Customer
+import com.example.onlineshop.data.remoteDataSource.RemoteDataSourceImpl
+import com.example.onlineshop.data.roomData.RoomDataSourceImpl
+import com.example.onlineshop.data.roomData.RoomService
+import com.example.onlineshop.data.sharedprefrences.MeDataSharedPrefrenceReposatory
+import com.example.onlineshop.databinding.FragmentLoginBinding
+import com.example.onlineshop.networkBase.NetworkChange
+import com.example.onlineshop.repository.RepositoryImpl
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 class LoginFragment : Fragment() {
+    companion object {
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
+
+    private lateinit var signinViewModel: LoginViewModel
+    private lateinit var binding: FragmentLoginBinding
+    private var email = ""
+    private var isLoginWithfirebase = false
+   // lateinit var callbackManager: CallbackManager
+    private lateinit var auth: FirebaseAuth
+    private lateinit var meDataSourceReo: MeDataSharedPrefrenceReposatory
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val application = requireNotNull(this.activity).application
+
+        val repository = RepositoryImpl(
+            RemoteDataSourceImpl(),
+            RoomDataSourceImpl(RoomService.getInstance(application))
+        )
+        val viewModelFactory = ViewModelFactory(repository, application)
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        signinViewModel =
+            ViewModelProvider(
+                this, viewModelFactory
+            ).get(LoginViewModel::class.java)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().toolbar.visibility = View.GONE
+        requireActivity().nav_view.visibility = View.GONE
+        meDataSourceReo = MeDataSharedPrefrenceReposatory(requireActivity())
+
+        signinViewModel.getCustomerList().observe(viewLifecycleOwner, Observer<List<Customer>?> {
+            Timber.i("we lodged")
+            Timber.i("Enas sigin in email" + email)
+            val customer: List<Customer> =
+                it.filter {
+                    it.email?.toLowerCase() ?: 0 == email
+                        .toLowerCase()
+                }
+
+            if (customer.isEmpty()) {
+                Toast.makeText(context, "you do not have an account", Toast.LENGTH_SHORT).show()
+            } else {
+                if (customer.get(0).note == binding.passwordEdt.text.toString() || isLoginWithfirebase) {
+
+                    meDataSourceReo.saveUsertState(true)
+                    meDataSourceReo.saveUsertId(customer[0].id.toString())
+                    meDataSourceReo.saveUsertName(customer[0].firstName.toString())
+                    Timber.i("we lodged")
+                  //  view.findNavController().currentBackStackEntry
+                    view.findNavController().popBackStack()
+                } else {
+                    Toast.makeText(context, "incorrect password", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        binding.loginBtn.setOnClickListener {
+            email = binding.emailEdt.text.toString().trim()
+            if (NetworkChange.isOnline) {
+                signinViewModel.getAllCustomers()
+            }
+            else {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.thereIsNoNetwork),
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+        }
+
+/*
+        binding.tvSignup.setOnClickListener {
+            view.findNavController()
+                .navigate(SignInFragmentDirections.actionSignInFragmentToSignUpFragment())
+        }*/
+
+        binding.googleButton.setOnClickListener {
+            val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+            launchSignInFlow(providers)
+        }
+
+        binding.facebookButton.setOnClickListener {
+            val providers = arrayListOf(AuthUI.IdpConfig.FacebookBuilder().build())
+            launchSignInFlow(providers)
+        }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Timber.i("Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Timber.i("Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+            observeAuthenticationState()
+        }
+    }
+
+    private fun launchSignInFlow(provider: List<AuthUI.IdpConfig>) {
+        // Create and launch sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_REQUEST_CODE
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(provider)
+                // .setIsSmartLockEnabled(false)
+                .build(), SIGN_IN_RESULT_CODE
+        )
+    }
+
+    private fun observeAuthenticationState() {
+        signinViewModel.authenticationState.observe(
+            viewLifecycleOwner,
+            Observer { authenticationState ->
+                when (authenticationState) {
+                    LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                        email = FirebaseAuth.getInstance().currentUser?.email.toString()
+                        if (NetworkChange.isOnline) {
+                            signinViewModel.getAllCustomers()
+                        } else {
+                            Toast.makeText(
+                                requireContext(),
+                                getString(R.string.thereIsNoNetwork),
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                        }
+                        isLoginWithfirebase = true
+                    }
+                    else -> {
+                        meDataSourceReo.saveUsertState(false)
+                    }
+                }
+            })
+    }
+
 /*
     private lateinit var loginViewModel: LoginViewModel
     private var _binding: FragmentLoginBinding? = null
